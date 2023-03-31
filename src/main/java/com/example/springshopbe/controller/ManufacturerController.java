67ad3665640aa97c3ce580f5bd6349fbd44e6581
DOhongson196 <109707579+DOhongson196@users.noreply.github.com>
2023-03-31 17:30:21 +0700

    add download logo for Manufacturer

--- src/main/java/com/example/springshopbe/controller/ManufacturerController.java
+++ src/main/java/com/example/springshopbe/controller/ManufacturerController.java
@@ -2,11 +2,15 @@ package com.example.springshopbe.controller;
 
 import com.example.springshopbe.domain.Manufacturer;
 import com.example.springshopbe.dto.ManufacturerDto;
+import com.example.springshopbe.exception.FileStorageException;
 import com.example.springshopbe.service.FileStorageService;
 import com.example.springshopbe.service.ManufacturerService;
 import com.example.springshopbe.service.MapValidationErrorService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -43,5 +47,25 @@ public class ManufacturerController {
         dto.setLogo(entity.getLogo());
 
         return new ResponseEntity<>(dto, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/logo/{filename:.+}")
+    public ResponseEntity<?> downloadFile(@PathVariable String filename, HttpServletRequest request){
+        Resource resource = fileStorageService.loadLogoFileAsResource(filename);
+        String contentType = null;
+        try{
+            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+        }catch (Exception ex){
+            throw new FileStorageException("File not found");
+        }
+        if(contentType == null){
+            contentType = "application/octet-stream";
+        }
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION,"attachment;filename=\""
+                +resource.getFilename() + "\"")
+                .body(resource);
     }
 }
